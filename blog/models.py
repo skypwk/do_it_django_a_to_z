c9ca074c9d02d6108bf53d15184eab3dc455669f
skypwkgamail <skypwk@gmail.com>
2022-03-22 12:45:17 +0900

    삭젝된 작성자는 None으로 표시하기

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
