9bd36213a1f7a7a19e8daba8a94d27ebe6f89ace
skypwkgamail <skypwk@gmail.com>
2022-03-23 14:56:50 +0900

    카테고리 페이지 만들기

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,9 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories'
 
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
+
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     #path('<int:pk>/', views.single_post_page),
     path('<int:pk>/', views.PostDetail.as_view()),
     # path('', views.index),
-    path('', views.PostList.as_view())
+    path('', views.PostList.as_view()),
+    path('category/<str:slug>/', views.category_page),
+
 
 ]
