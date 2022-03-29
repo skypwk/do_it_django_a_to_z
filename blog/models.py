3e708cb7f6ed1bfebb884fd6194f5d67ce1fb8f0
skypwkgamail <skypwk@gmail.com>
2022-03-29 10:45:54 +0900

    사용자 아바타 보여주기

--- blog/models.py
+++ blog/models.py
@@ -80,6 +80,12 @@ class Comment(models.Model):
     def get_absolute_url(self):
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return 'https://avatars.dicebear.com/api/adventurer/woon.svg'
+
 
 
 
